7e6f520f4219941973ab661a1bb4af4b93d0fc9d
souravkh <souravkhatri1996@gmail.com>
2026-04-05 20:54:42 +0530

    Update the code

--- Pattern.java
+++ Pattern.java
@@ -1,13 +1,13 @@
 class Pattern{
   public static void main(String[] args){
-	for(int i = 0 ; i<=5;i++)
+	for(int i = 0 ; i<=4;i++)
 	{for(int j=0 ; j<=i;j++)
 		{
 		System.out.print("*");
 		} 
 	System.out.println("");
 	}
-	for(int i = 0 ; i<=5;i++)
+	for(int i = 0 ; i<=4;i++)
 	{for(int j=0 ; j<=i;j++)
 		{
 		System.out.print(j);
